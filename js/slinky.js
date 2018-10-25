79853876948995bdca823d614bee0cb2c55a9e8a
aemartos <aemartos@gmail.com>
2018-10-25 11:27:30 +0200

    Minor changes

--- js/slinky.js
+++ js/slinky.js
@@ -18,6 +18,7 @@ User.prototype = Object.create(Form.prototype);
 User.prototype.constructor = User;
 
 User.prototype.initUser = function () {
+  this.points = 10000;
   let pos = this.getFreePosition({x:40, y:25},100);
   this.x = pos.x;
   this.y = pos.y;
